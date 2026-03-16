d236b935573f1c2bc03476922608da89d3202802
micromex16 <micromex16@gmail.com>
2026-03-16 12:42:47 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'condor-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
